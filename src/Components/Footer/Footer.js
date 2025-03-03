8a45703e850adff33c41cccd797ea5a1cfa0131a
Sahil Singh Fartyal <97732098+sahilf637@users.noreply.github.com>
2025-03-03 14:06:22 +0530

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,21 +1,29 @@
-import GitHubButton from 'react-github-btn'
-
+import GitHubButton from "react-github-btn";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-
-
-            <div className='copyright'>
-                <p className='px'> If you found this useful, you can buy me a coffee ☕️ or give a follow ✨ </p>
-                <GitHubButton href="https://github.com/raghavpatidar" data-color-scheme="no-preference: dark; light: light_high_contrast; dark: dark_high_contrast;" data-size="small" aria-label="Follow @raghavpatidar on GitHub">Follow </GitHubButton>
-
-            </div>
-            <div className='copyright'>
-                <p className='px'> ©  Raghav Patidar.</p>
-                <img src="https://visitor-badge.glitch.me/badge?page_id=cfladerraghav"></img>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <div className="copyright">
+        <p className="px">
+          {" "}
+          If you found this useful, you can buy me a coffee ☕️ or give a follow
+          ✨{" "}
+        </p>
+        <GitHubButton
+          href="https://github.com/sahilf637"
+          data-color-scheme="no-preference: doark; light: light_high_contrast; dark: dark_high_contrast;"
+          data-size="small"
+          aria-label="Follow @sahilf637 on GitHub"
+        >
+          Follow{" "}
+        </GitHubButton>
+      </div>
+      <div className="copyright">
+        <p className="px"> Sahil Singh Fartyal.</p>
+        <img src="https://visitor-badge.glitch.me/badge?page_id=cfladerraghav"></img>
+      </div>
+    </footer>
+  );
+};
 export default Footer;
+
